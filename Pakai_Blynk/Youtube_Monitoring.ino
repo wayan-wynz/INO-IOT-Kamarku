0e8ac40fd84c31f4b7d2836d42843676cc848ce8
I Wayan Sujane <79820713+wayan-wynz@users.noreply.github.com>
2023-05-15 12:48:32 +0700

    Update Youtube_Monitoring.ino
    Perapian pada Serial.println

--- Pakai_Blynk/Youtube_Monitoring.ino
+++ Pakai_Blynk/Youtube_Monitoring.ino
@@ -14,28 +14,28 @@ void youtubesubs()
     if(api.getChannelStatistics(CHANNEL_ID))
     {
       lcd.clear();
-      Serial.println("---------Stats---------");
-      Serial.print("Subscriber Count: ");
-      Serial.println(api.channelStats.subscriberCount);
+//      Serial.println("---------Stats---------");
+//      Serial.print("Subscriber Count: ");
+//      Serial.println(api.channelStats.subscriberCount);
       lcd.print(0, 0,(String("Subscribe: ") + String(api.channelStats.subscriberCount)));
       Blynk.virtualWrite(V6, api.channelStats.subscriberCount);
       
-      Serial.print("Total Penayangan: ");
-      Serial.println(api.channelStats.viewCount);
+//      Serial.print("Total Penayangan: ");
+//      Serial.println(api.channelStats.viewCount);
       Blynk.virtualWrite(V7, api.channelStats.viewCount);
       
-      Serial.print("Total Komentar: ");
-      Serial.println(api.channelStats.commentCount);
+//      Serial.print("Total Komentar: ");
+//      Serial.println(api.channelStats.commentCount);
       Blynk.virtualWrite(V8, api.channelStats.commentCount);
       
-      Serial.print("Total Video: ");
-      Serial.println(api.channelStats.videoCount);
+//      Serial.print("Total Video: ");
+//      Serial.println(api.channelStats.videoCount);
       lcd.print(0, 1,(String("All Video: ") + String(api.channelStats.videoCount)));
       Blynk.virtualWrite(V9, api.channelStats.videoCount);
       // Probably not needed :)
-      Serial.print("hiddenSubscriberCount: ");
-      Serial.println(api.channelStats.hiddenSubscriberCount);
-      Serial.println("------------------------");
+//      Serial.print("hiddenSubscriberCount: ");
+//      Serial.println(api.channelStats.hiddenSubscriberCount);
+//      Serial.println("------------------------");
     }
 //    delay(100);
     nextRunTime = millis() + timeBetweenRequests;
